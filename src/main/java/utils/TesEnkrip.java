77e89bd58777d86b8f09af9d09d7227129c3ae04
almassyafina <almasjaufilael.25@gmail.com>
2026-06-12 20:23:53 +0700

    Enskripsi dan Deskripsi

---
+++ src/main/java/utils/TesEnkrip.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+/**
+ *
+ * @author ASUS
+ */
+public class TesEnkrip {
+    public static void main(String[] args) {
+        String pwd = Encryption
+                .encrypt("123");
+        System.out.println(pwd);
+    }
+}
